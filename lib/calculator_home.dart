6cea50bc3f2130315df268b2a6c0506ec84f23ba
Donnubzy <donnubzy@gmail.com>
2025-12-26 19:44:46 +0100

    Modified the Calculators main screen

--- lib/calculator_home.dart
+++ lib/calculator_home.dart
@@ -1,9 +1,6 @@
 
-import 'package:calculator/model/provider.dart';
-import 'package:calculator/util/styling.dart';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'model/calc_logic.dart';
 import 'package:calculator/ui/views/details_screen.dart';
 import 'package:calculator/ui/views/buttons_view.dart';
 
@@ -12,24 +9,17 @@ class Calculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var read = context.read<CalculatorModel>();
-    var watch = context.read<CalculatorModel>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Calculator App"),
       ),
       backgroundColor: Colors.grey.shade50,
-      body: SafeArea(
+      body: const SafeArea(
         child: Column(
           children: <Widget>[
-            DetailsScreen(
-                details: watch.display
-            ),
-            ButtonsView(
-                buttonCallback: read.buttonsCallback,
-                isInverse: watch.isNatural
-            ),
+            DetailsScreen(),
+            ButtonsView(),
           ],
         ),
       ),
